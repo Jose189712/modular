4a81fac7cfb4ea93af28c6faa3bb131c4ab44471
unknown <jossre9@gmail.com>
2019-12-08 11:32:16 -0600

    Revision de registros

--- alumnos/alumnosR.php
+++ alumnos/alumnosR.php
@@ -10,9 +10,9 @@
             if($this->$db != NULL){            
                 $statement = $this->$db->prepare("SELECT name_user FROM alumnos WHERE name_user='$name_user'");
                 $statement->execute();
-                if($statement){
-                    $respuesta = $statement->fetchAll();
-                    return $respuesta;
+                $respuesta = $statement->fetchAll();                
+                if(empty($respuesta)){                    
+                    return 'true';
                 }else{
                     return 'false';
                 } 
@@ -21,10 +21,10 @@
             }               
         }//fin del método empty_Alumno
         //Método para registrar a un alumno por el mismo
-        public function set_alumnos($name_user,$nombre,$apellidoP,$apellidoM,$password,$sexo){
+        public function set_alumnos($name_user,$nombre,$apellidoP,$apellidoM,$password,$sexo){            
             $auxiliar_db = $this->$db->prepare("INSERT INTO alumnos(nombreAlumno,apellido_p_Alumno,apellido_m_Alumno,name_user,palabra_secreta,sexo)
-            VALUES('$nombre','$apellidoP','$apellidoM','$name_user','$password','$sexo')");
-            $h = $auxiliar_db->execute();  
+            VALUES('$nombre','$apellidoP','$apellidoM','$name_user','$password','$sexo')");            
+            $h = $auxiliar_db->execute();              
             return $h;          
         }//método para registrar a los alumnos 
     }//fin de la clase AlumnoR
--- alumnos/registro.php
+++ alumnos/registro.php
@@ -7,17 +7,15 @@
     $password = $_POST['password'];
     $sexo = $_POST['sexo'];
 
-    $registro = new AlumnoR();
-
-    //var_dump($registro->empty_Alumno($alumno));
-    
+    $registro = new AlumnoR();    
 
     if($registro->empty_Alumno($alumno) == 'false'){
-        echo 'fallo';
-    }else if(empty($registro->empty_Alumno($alumno))){
+        echo 'Fallo registro nombre de usuario existe';
+    }else if(empty($registro->empty_Alumno($alumno)) == 'Error en la conexión'){
+        echo 'Error en la conexión a la base de datos';
+    }
+    else{
         var_dump($registro->set_alumnos($alumno,$nombre,$apellidoP,$apellidoM,$password,$sexo));
         echo 'Exito';
-    }else{
-        echo 'existe';
-    }//Fin del if else para controlar los registro repetidos
+    }
 ?>
