ac6fc51753148f944e5c2e6c39761631d836fbdd
Jose189712 <jossre9@gmail.com>
2021-06-19 11:01:48 -0500

    Cambios iniciales tercera fase

--- config/configDB.php
+++ config/configDB.php
@@ -1,6 +1,6 @@
 <?php
 define("servidor","localhost");
 define("baseD","seducativo");
-define("usuario","root");
-define("password","18joRODRI");
+define("usuario","jose");
+define("password","jose1234");
 ?>
